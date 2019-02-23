53b90b63e863c9c37457c7be7a556a03e14134a3
kaiz711 <kaiz711@naver.com>
2019-02-23 18:51:37 +0900

    .

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import blog.views
 
 urlpatterns = [
+    path('', blog.views.blog, name="blog"),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
